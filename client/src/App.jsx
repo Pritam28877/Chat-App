5318284a72b7542699f9deab6a807885815c78b6
Pritam Das <dpritam28577@gmail.com>
2023-08-26 14:19:50 +0530

    install the tailwind css in the project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div>
-        <h1>hello</h1>
-      </div>
+      <h1 className=" font-bold underline">Hello world!</h1>
     </>
   );
 }
