571cb6c8b64e5d1a181313450cada434a895c72f
Pritam Das <dpritam28577@gmail.com>
2023-08-27 02:23:47 +0530

    the styleing for the registation is done

--- client/src/Components/Register/Register.jsx
+++ client/src/Components/Register/Register.jsx
@@ -2,23 +2,25 @@ import React from "react";
 
 const Register = () => {
   return (
-    <div className="bg-blue-50 h-screen">
-      <form action="" className="w-64 mx-auto">
+    <div className="bg-blue-50 h-screen flex items-center">
+      <form action="" className="w-64 mx-auto ">
         <input
-          className="block w-full"
+          className="block w-full rounded-sm p-3 mb-2 border"
           type="text"
           name=""
           id=""
           placeholder="Username"
         />
         <input
-          className="block w-full"
+          className="block w-full rounded-sm  p-3 mb-2 border"
           type="password"
           name=""
           id=""
           placeholder="password"
         />
-        <button className="bg-blue-500">Register</button>
+        <button className="bg-blue-500 text-white w-full rounded-sm p-2">
+          Register
+        </button>
       </form>
     </div>
   );
