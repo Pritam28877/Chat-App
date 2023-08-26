3e84a8aa7945959c482e753cf66c6a0b2e77f162
Pritam Das <dpritam28577@gmail.com>
2023-08-27 03:15:18 +0530

    the mvc folder arr created to the server folder

--- server/Index.js
+++ server/Index.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const app = express();
+const server = require("./routes/server");
+
+app.get("/", (req, res) => {
+  res.json("server ok");
+});
+app.use("/register", server);
 
 app.listen(8000, (e) => {
   if (e) {
