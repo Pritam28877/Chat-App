1f36782b97f9aaa9ed620c80c6be7f12beeacf0f
Pritam Das <dpritam28577@gmail.com>
2023-08-27 02:42:23 +0530

    add the backend express server

---
+++ server/Index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get()
+
+app.listen(8000, (error) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log(`server is running on port 8000`);
+});
