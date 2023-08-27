a52a188a60d75e418846088d790c33251f7f916a
Pritam Das <dpritam28577@gmail.com>
2023-08-27 20:00:36 +0530

    add the function for the jwt token sending

--- server/Index.js
+++ server/Index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 const db = require("./config/mongodb");
 const server = require("./routes/server");
-db();
+// db();
 app.get("/", (req, res) => {
   res.json("server ok");
 });
--- server/controllers/register.js
+++ server/controllers/register.js
@@ -1,3 +1,22 @@
-module.exports.register = (req, res) => {
-  res.json("server ok2");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+//* the variable for  the token
+const maxage = 3 * 24 * 60 * 60;
+const jwtSecret = process.env.JWT_SECRET;
+
+console.log(jwtSecret);
+
+//* jwt webtoken creation
+const createToke = (id) => {
+  return jwt.sign({ id }, jwtSecret, {
+    expiresIn: maxage,
+  });
+};
+
+//* the post action on the user create action
+module.exports.register = async (req, res) => {
+  const { user, password } = req.body;
+  const createdNewUser = await User.create({ username, password });
+  const token = createToke(createdNewUser._id);
 };
