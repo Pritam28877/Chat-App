6d48fb3051e50d6178a97b5161432f5415e04849
Pritam Das <dpritam28577@gmail.com>
2023-10-10 20:23:04 +0530

    change the style to cover the full style of the chat

--- client/src/Components/Chat/Chat.jsx
+++ client/src/Components/Chat/Chat.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Chat = () => {
   return (
     <div className="flex h-screen ">
-      <div className="bg-blue-100">left</div>
-      <div className="bg-blue-300"></div>
+      <div className="bg-blue-100 w-1/3">left</div>
+      <div className="bg-blue-300 w-2/3">center</div>
     </div>
   );
 };
