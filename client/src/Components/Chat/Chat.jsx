e03d3f5532e34c2c8ee3e794f97f998ad3ce659d
Pritam Das <dpritam28577@gmail.com>
2023-10-10 21:37:06 +0530

    add the send icon jsx

--- client/src/Components/Chat/Chat.jsx
+++ client/src/Components/Chat/Chat.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import Send from "../../icons/send";
 
 const Chat = () => {
   return (
     <div className="flex h-screen ">
       <div className="bg-blue-100 w-1/3">left</div>
-      <div className="bg-blue-300 w-2/3">center</div>
+      <div className="bg-blue-300 w-2/3">
+        <div>messages with selected person</div>
+        <div>
+          <input
+            type="text"
+            placeholder="type your messages"
+            className="bg-white border p-2"
+          />
+          <button className="">
+            <Send />
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
