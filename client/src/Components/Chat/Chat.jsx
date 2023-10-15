5de7ed0f8c6907eb779d354656e8fee1ab55c7f1
Pritam Das <dpritam28577@gmail.com>
2023-10-15 14:35:48 +0530

    add the function  to show user online

--- client/src/Components/Chat/Chat.jsx
+++ client/src/Components/Chat/Chat.jsx
@@ -3,14 +3,20 @@ import Send from "../../icons/send";
 
 const Chat = () => {
   const [messages, setMessages] = useState();
+  const [online, setOnline] = useState({});
   useEffect(() => {
     const ws = new WebSocket("ws://localhost:8000");
     setMessages(ws);
     ws.addEventListener("message", handleMessage);
   }, []);
 
-  const showOnline = (person) => {
-    console.log(person);
+  const showOnline = (personArray) => {
+    // console.log(personArray);
+    const People = {};
+    personArray.forEach(({ id, user }) => {
+      People[id] = user;
+    });
+    setOnline(People);
   };
   const handleMessage = (e) => {
     const messagedata = JSON.parse(e.data);
@@ -21,7 +27,14 @@ const Chat = () => {
   };
   return (
     <div className="flex h-screen ">
-      <div className="bg-blue-100 w-1/3">contacts</div>
+      <div className="bg-blue-100 w-1/3">
+        {Object.keys(online).map((id) => (
+          <div key={id} className="flex gap-2 p-2">
+            <div>{online[id]}</div>
+            <div></div>
+          </div>
+        ))}
+      </div>
       <div className="flex flex-col bg-blue-300 w-2/3 p-2">
         <div className="flex-grow">messages with selected person</div>
         <div className="flex gap-2 mx-2">
